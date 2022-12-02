fcb836463801c7d1f9f3f4c00a2f4776539f478f
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-12-02 13:17:36 +0900

    Update The-Hurdle-Race.cpp

--- The-Hurdle-Race.cpp
+++ The-Hurdle-Race.cpp
@@ -1,3 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'hurdleRace' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY height
+ */
+
 int hurdleRace(int k, vector<int> height)
 {
     return std::max(*std::max_element(height.begin(), height.end())-k, 0);
