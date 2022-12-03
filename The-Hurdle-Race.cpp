6a8d849f1c13cd6db2ad8bcd33f98c102f3b93b1
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-12-03 17:41:03 +0900

    Update The-Hurdle-Race.cpp

--- The-Hurdle-Race.cpp
+++ The-Hurdle-Race.cpp
@@ -15,16 +15,81 @@ vector<string> split(const string &);
  *  2. INTEGER_ARRAY height
  */
 
-int hurdleRace(int k, vector<int> height)
-{
-    return std::max(*std::max_element(height.begin(), height.end())-k, 0);
+int hurdleRace(int k, vector<int> height) {
+    int max_value = *std::max_element(height.cbegin(), height.cend()) - k;
+    return std::max(max_value, 0);
 }
 
-int hurdleRace2(int k, vector<int> height)
+int main()
 {
-    int result = 0;
-    int max = *std::max_element(height.begin(), height.end());
-    result = std::max(max-k, 0);
+    ofstream fout(getenv("OUTPUT_PATH"));
 
-    return result;
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int k = stoi(first_multiple_input[1]);
+
+    string height_temp_temp;
+    getline(cin, height_temp_temp);
+
+    vector<string> height_temp = split(rtrim(height_temp_temp));
+
+    vector<int> height(n);
+
+    for (int i = 0; i < n; i++) {
+        int height_item = stoi(height_temp[i]);
+
+        height[i] = height_item;
+    }
+
+    int result = hurdleRace(k, height);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
 }
